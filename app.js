dd4241b4f9ee398086d1340897cf318d388c0811
Chris Huey <chrishuey@Wonolos-MacBook-Air.local>
2018-05-30 08:30:28 -0700

    added npm packages request and express

--- app.js
+++ app.js
@@ -11,7 +11,38 @@ console.log("Starting app!");
 // adds to package.json file
 
 const _ = require('lodash');
+const request = require('request');
 
 // nodemon <root file name>.js
 
 // JSON.stringify(<obj>);
+// JSON.parse(<string>);
+
+
+// **** CALLBACKS **** //
+
+// var getUser = (id, callback) => {
+
+// 	var user = {
+// 		id: id,
+// 		name: 'Charlie',
+// 	};
+// 	callback(user);
+
+// };
+
+// getUser(31, (userObj) => {
+// 	// do something with data => user
+// 	console.log(userObj);
+// })
+
+
+// make the request to Google API
+request({
+	url: geocodeUrl,
+	json: true
+}, (error, response, body) => {
+	console.log("printing body: " + JSON.stringify(body, undefined, 2));
+})
+
+// how to grab the form values and use them in app.js
