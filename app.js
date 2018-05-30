03f76717195f7352c546dc4905f637f10ba7e62f
Chris Huey <chrishuey@Wonolos-MacBook-Air.local>
2018-05-30 07:59:42 -0700

    added node and nodemon

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Starting app!");
+
+// require() 
+// - available to you whenever
+// ex. const fs = require('fs');
+
+// const x = require("./x.js");
+// will bring in any code from "x.js"
+
+// npm install <package name> --save
+// adds to package.json file
+
+const _ = require('lodash');
+
+// nodemon <root file name>.js
+
+// JSON.stringify(<obj>);
