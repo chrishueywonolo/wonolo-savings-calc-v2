f1889738022a7dc0da36476a2ff1c0c41928bad1
Chris Huey <chrishuey@chrishuey.wonolo.lan>
2018-06-11 07:39:21 -0700

    added path.join to static

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + 'public')));
 
 
 
