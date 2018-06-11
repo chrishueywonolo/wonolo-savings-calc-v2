f2f93e052d261deadbd3b53849f770b6d82143aa
Chris Huey <chrishuey@chrishuey.wonolo.lan>
2018-06-11 07:44:09 -0700

    added / again

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 
 
